1439d8e885b82c5ba3f5380f5559059f77eaad9a
Han <allen0346@gmail.com>
2021-08-23 14:50:58 +0800

    Adjust: add test router

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const app = express();
-const { signInRouter, signoutRouter } = require('./routers');
+const { signInRouter, signoutRouter, testRouter } = require('./routers');
 const { cors } = require('./middlewares');
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
 
 app.use(cors);
 app.use(cookieParser());
 app.use(express.json());
 app.use('/user', signInRouter);
 app.use('/user', signoutRouter);
+app.use('/test', testRouter);
 
 module.exports = app;
--- src/routers/index.js
+++ src/routers/index.js
@@ -1,9 +1,11 @@
 const oauthRouter = require('./oauth');
 const signInRouter = require('./sign-in');
 const signoutRouter = require('./signout');
+const testRouter = require('./test');
 
 module.exports = {
   oauthRouter,
   signInRouter,
-  signoutRouter
+  signoutRouter,
+  testRouter
 };
