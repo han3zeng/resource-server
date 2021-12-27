35d2a707f6584994559f96dc1193c3f5c6f33aef
Han <allen0346@gmail.com>
2021-12-27 15:36:03 +0800

    Adjust: modify setting for samesite to accommodate sign-in redirect scenario

--- src/routers/authentication/sign-in.js
+++ src/routers/authentication/sign-in.js
@@ -35,7 +35,7 @@ const signInComposer = ({
       .cookie('accessToken', accessToken, {
         domain,
         httpOnly: true,
-        sameSite: config.nodeEnv === 'production' ? 'none' : 'lax',
+        sameSite: 'lax',
         secure: config.nodeEnv === 'production',
         path: '/',
         expires: expireDate()
@@ -43,7 +43,7 @@ const signInComposer = ({
       .cookie('user-profile', userProfile, {
         clientDomain,
         httpOnly: false,
-        sameSite: config.nodeEnv === 'production' ? 'strict' : 'lax',
+        sameSite: 'lax',
         secure: config.nodeEnv === 'production',
         path: '/',
         expires: expireDate(),
